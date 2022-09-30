c5f57c535e99999ed28a53c804f35ec179b56f70
Alessio Pintus <alessio.pintus@gmail.com>
2022-09-30 11:12:37 +0200

    ENG-4241 Update labels

--- ui/widgets/widgets-dir/strapi-config-app/src/helpers/constants.js
+++ ui/widgets/widgets-dir/strapi-config-app/src/helpers/constants.js
@@ -1,8 +1,8 @@
 
-export const TOOLTIP_URL = 'URL of the application, Example - http://20.113.131.137:1337 \n http://quickstart.192.168.43.3.nip.io/entando-strapi \n http://hubdev.okd-entando.org/entando-strapi'
-export const LABEL_APPLICATION_URL = 'Application URL'
-export const MSG_REQ_APPLICATION_URL = 'Application URL is Required'
-export const MSG_VALID_APPLICATION_URL = 'Enter a valid application URL'
-export const LABEL_STRAPI_CONFIG_SETTINGS = 'Strapi config setting'
+export const TOOLTIP_URL = 'URL of the Strapi application, Example - http://20.113.131.137:1337 \n http://quickstart.192.168.43.3.nip.io/entando-strapi \n http://hubdev.okd-entando.org/entando-strapi'
+export const LABEL_APPLICATION_URL = 'Strapi URL'
+export const MSG_REQ_APPLICATION_URL = 'Strapi URL is Required'
+export const MSG_VALID_APPLICATION_URL = 'Enter a valid Strapi URL'
+export const LABEL_STRAPI_CONFIG_SETTINGS = 'Strapi setting'
 export const BUTTON_SAVE = 'Save'
 export const VALID_URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
